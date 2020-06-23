1d1bea6b379f9f17e156871c266d7e5cf31b5f63
Aaron Horowitz <ahorowitz@acluorg>
2020-06-23 10:27:59 -0700

    documenting resources

--- R/resources.R
+++ R/resources.R
@@ -1,4 +1,16 @@
-## Resources
+#' Review Resources
+#'
+#' This function series provides easy file access
+#' code for resources in the `LibLabReviews` package.
+#'
+#' @param type the type of resource you are selecting. A limited option set.
+#' @param ... Additional arguments passed on to `system.file` function in LibLabReviews
+#'
+#' @return The correct file path for the associated resource
+#' @export
+#' @examples
+#' #get file path for the generic pull request template
+#' pull_resources(type = "analysis", "pull_request_template.md")
 review_resources <- function(type = "analysis", ...) {
 
   type = match.arg(type)
@@ -8,6 +20,8 @@ review_resources <- function(type = "analysis", ...) {
   )
 }
 
+#' @export
+#' @rdname review_resources
 issue_resources <- function(type = "analysis",...){
 
   type = match.arg(type)
@@ -15,6 +29,8 @@ issue_resources <- function(type = "analysis",...){
   review_resources(type = type, "ISSUE_TEMPLATE",...)
 }
 
+#' @export
+#' @rdname review_resources
 pull_resources <- function(type = "analysis",...){
 
   type = match.arg(type)

388e16eabe58a68218f1b2fd5a078c9e84af7b85
Aaron Horowitz <ahorowitz@acluorg>
2020-06-29 19:47:17 -0700

    additional cli functions

--- R/utils-cli.R
+++ R/utils-cli.R
@@ -1,4 +1,4 @@
-#' Print clean done text during project setup
+#' Print clean cli information text
 #'
 #' @param ... Any params usable by \code{\link[glue]{glue}}
 #' @param .envir Environment
@@ -11,33 +11,40 @@
 done_line <- function (..., .envir = parent.frame())
 {
   out <- glue::glue(..., .envir = .envir)
-  cli::cat_line(checkize(out))
+  cli::cat_line(check_ize(out))
 }
 
-#' Print clean warning text during project setup
-#'
-#' @param ... Any params usable by \code{\link[glue]{glue}}
-#' @param .envir Environment
-#'
-#' @return invisible
 #' @export
-#'
+#' @rdname done_line
 #' @examples
 #' warning_line("wooo....bullets")
 warning_line <- function(..., .envir = parent.frame()){
   out <- glue::glue(..., .envir = .envir)
-  cli::cat_line(Xize(out))
+  cli::cat_line(x_ize(out))
 }
 
-bulletize <- function (line, bullet = "*")
+#' @export
+#' @rdname done_line
+#' @examples
+#' info_line("wooo....bullets")
+info_line <- function(..., .envir = parent.frame()){
+  out <- glue::glue(..., .envir = .envir)
+  cli::cat_line(info_ize(out))
+}
+
+bullet_ize <- function (line, bullet = "*")
 {
   paste0(bullet, " ", line)
 }
 
-checkize <- function(line){
-  bulletize(line,bullet = crayon::green(clisymbols::symbol$tick))
+info_ize <- function(line){
+  bullet_ize(line,bullet = crayon::blue(clisymbols::symbol$info))
 }
 
-Xize <- function(line){
-  bulletize(line,bullet = crayon::red(clisymbols::symbol$cross))
+check_ize <- function(line){
+  bullet_ize(line,bullet = crayon::green(clisymbols::symbol$tick))
+}
+
+x_ize <- function(line){
+  bullet_ize(line,bullet = crayon::red(clisymbols::symbol$cross))
 }

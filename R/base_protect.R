6a564bb50d374b4fcf10d7ee2af18634683901a2
Aaron Horowitz <ahorowitz@acluorg>
2020-08-09 15:05:05 -0700

    nice effiient add_branch_protetion function with some basic options enabled. More advanced options not set up at this time.

--- R/base_protect.R
+++ R/base_protect.R
@@ -43,3 +43,43 @@ is_master_protected <- function(repo_spec = github_repo_spec(), auth_token = git
                         host = host
                         )
 }
+
+add_branch_protection <- function(branch = "master",
+                                  required_approving_review_count = 1,
+                                  required_linear_history = TRUE,
+                                  allow_force_pushes = FALSE,
+                                  allow_deletions = FALSE,
+                                  enforce_admins = TRUE,
+                                  repo_spec = github_repo_spec(),
+                                  auth_token = github_token(),
+                                  host = NULL
+){
+
+  gh <- base_gh(repo_spec,auth_token,host, accept_header = "application/vnd.github.luke-cage-preview+json")
+  safe_gh <- purrr::safely(gh)
+  owner = usethis:::spec_owner(repo_spec)
+  repo = usethis:::spec_repo(repo_spec)
+  put_text <- glue::glue("PUT /repos/{owner}/{repo}/branches/{branch}/protection")
+  res <- safe_gh(put_text,
+               required_status_checks = NA,
+               enforce_admins = enforce_admins,
+               required_pull_request_reviews = list(
+                 dismissal_restrictions = list(
+                  users = list(),
+                  teams = list()
+                 ),
+                 dismiss_stale_reviews = TRUE,
+                 require_code_owner_reviews = FALSE,
+                 required_approving_review_count = required_approving_review_count
+                ),
+               restrictions = NA,
+               required_linear_history = required_linear_history,
+               allow_force_pushes = allow_force_pushes,
+               allow_deletions = allow_deletions
+          )
+
+  if(!is.null(res$error))
+    stop(res$error)
+
+  return(invisible(res$result))
+}
